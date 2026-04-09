820529cd565a7adeeac0faa088d84974207f4ef2
Aspen <aspen.ritchie@gmail.com>
2026-04-09 15:28:48 -0700

    fix: remove unused imports and update line length limit

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import os
-
 import pandas as pd
-from fastapi import FastAPI
 from pydantic import BaseModel, Field
 
 from ml.data import apply_label, process_data
-from ml.model import inference, load_model
 
 # DO NOT MODIFY
+
+
 class Data(BaseModel):
     age: int = Field(..., example=37)
     workclass: str = Field(..., example="Private")
@@ -24,18 +22,23 @@ class Data(BaseModel):
     capital_gain: int = Field(..., example=0, alias="capital-gain")
     capital_loss: int = Field(..., example=0, alias="capital-loss")
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
-    native_country: str = Field(..., example="United-States", alias="native-country")
+    native_country: str = Field(...,
+                                example="United-States",
+                                alias="native-country")
 
-path = None # TODO: enter the path for the saved encoder 
+
+path = None  # TODO: enter the path for the saved encoder
 encoder = load_model(path)
 
-path = None # TODO: enter the path for the saved model 
+path = None  # TODO: enter the path for the saved model
 model = load_model(path)
 
 # TODO: create a RESTful API using FastAPI
-app = None # your code here
+app = None  # your code here
 
 # TODO: create a GET on the root giving a welcome message
+
+
 @app.get("/")
 async def get_root():
     """ Say hello!"""
@@ -54,21 +57,11 @@ async def post_inference(data: Data):
     data = {k.replace("_", "-"): [v] for k, v in data_dict.items()}
     data = pd.DataFrame.from_dict(data)
 
-    cat_features = [
-        "workclass",
-        "education",
-        "marital-status",
-        "occupation",
-        "relationship",
-        "race",
-        "sex",
-        "native-country",
-    ]
     data_processed, _, _, _ = process_data(
         # your code here
         # use data as data input
         # use training = False
         # do not need to pass lb as input
     )
-    _inference = None # your code here to predict the result using data_processed
+    _inference = None  # your code here to predict the result using data_processed
     return {"result": apply_label(_inference)}
--- train_model.py
+++ train_model.py
@@ -1,16 +1,12 @@
 import os
 
-import pandas as pd
-from sklearn.model_selection import train_test_split
 
 from ml.data import process_data
 from ml.model import (
     compute_model_metrics,
-    inference,
     load_model,
     performance_on_categorical_slice,
     save_model,
-    train_model,
 )
 # TODO: load the cencus.csv data
 project_path = "Your path here"
