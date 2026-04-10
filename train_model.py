bdbb99da686b5369a9c7aba269678a7d0aa90f41
Aspen <aspen.ritchie@gmail.com>
2026-04-09 17:13:01 -0700

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -78,22 +78,20 @@ p, r, fb = compute_model_metrics(y_test, preds)
 print(f"Precision: {p:.4f} | Recall: {r:.4f} | F1: {fb:.4f}")
 
 # TODO: compute the performance on model slices using the performance_on_categorical_slice function
-# Clear the file if it already exists before appending
 if os.path.exists("slice_output.txt"):
     os.remove("slice_output.txt")
 
-# iterate through the categorical features
 for col in cat_features:
     for slicevalue in sorted(test[col].unique()):
         count = test[test[col] == slicevalue].shape[0]
         p, r, fb = performance_on_categorical_slice(
-            test, 
-            col, 
-            slicevalue, 
-            cat_features, 
-            "salary", 
-            encoder, 
-            lb, 
+            test,
+            col,
+            slicevalue,
+            cat_features,
+            "salary",
+            encoder,
+            lb,
             model
         )
         with open("slice_output.txt", "a") as f:
