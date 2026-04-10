dd3daa401fcc0e231d216e7cdcc0773fd868be39
Aspen <aspen.ritchie@gmail.com>
2026-04-09 17:02:49 -0700

    Update model.py

--- ml/model.py
+++ ml/model.py
@@ -126,9 +126,7 @@ def performance_on_categorical_slice(
     recall : float
     fbeta : float
 
-    """
-    # TODO: implement the function
-    
+    """    
     slice_df = data[data[column_name] == slice_value]
             
     X_slice, y_slice, _, _ = process_data(
